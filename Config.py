87b6c539f6bbd49ee98ad8d08ced80a08dae03b3
gangsta0 <76583019+gangsta0@users.noreply.github.com>
2023-03-04 10:01:18 +0400

    Update Config.py

--- Config.py
+++ Config.py
@@ -7,5 +7,5 @@ class Config():
     admins = {}
     API_ID = int(os.environ.get("API_ID","15954332"))
     API_HASH = os.environ.get("API_HASH","85adea6f1eaf068b707703b4846a9ced")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5854631326:AAFgYhVxGd5NHGMJmAx4LVn7QAhtoYptlIs")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5852449542:AAFzQFmPsVBh74KvKOemUFGnTbcZzd6Q5iU")
     
